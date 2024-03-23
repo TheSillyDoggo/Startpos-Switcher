75c5b1faaa2aa2d109dd03a5120767b47fa39f6e
Explodingbill <talgood09@gmail.com>
2024-03-23 13:45:40 +1100

    fffffffffffffffff

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,12 @@ void switchToStartpos(int incBy, bool actuallySwitch = true)
         StartPosObject* startPosObject = selectedStartpos == -1 ? nullptr : startPos[selectedStartpos];
 
         // delete the startposcheckpoint (see playlayer_resetlevel line 148 in ida)
+        #ifdef GEODE_IS_WINDOWS
         int offset = 0xB85;// 0xA6A;
-
-        #ifdef GEODE_IS_ANDROID32
-        offset = 0xb93;
+        #endif
+        
+        #ifdef GEODE_IS_ANDROID
+        int offset = 0xB93;
         #endif
 
         int* startPosCheckpoint = (int*)GameManager::get()->getPlayLayer() + offset;//2949
